532817a225ac04f4c71e5b23c867dd7839f16d04
Cabr1to <luis.b.c.melo@gmail.com>
2025-03-08 11:07:53 -0300

    ex 2

--- Aula_08_03_2025/script.js
+++ Aula_08_03_2025/script.js
@@ -17,3 +17,10 @@ console.log(idade)
 console.log(typeof idade)
 
 console.log(`Voce se chama ${nomeCompleto} e tem ${idade} anos`)
+
+console.log(5 == "5")
+console.log(5 === "5")
+
+console.log(5 !== "5")
+
+
---
+++ Aula_08_03_2025/script01.js
@@ -0,0 +1,6 @@
+n = prompt("digite um numero")
+let string = ""
+for (let i = 0; i < n; i++){
+    string += "*"
+    console.log(string)
+}
